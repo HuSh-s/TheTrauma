403c6ccfa1be23982d1af2eadb0ec79cf8a36704
HuSh-s <semihbay17@gmail.com>
2025-07-08 23:28:37 +0300

    Added Vehicle Exit Functionality to Driving System
    - Implemented player exit mechanic from vehicle triggered by Interact button
    - On exit, player position and rotation set to predefined ExitPoint
    - Reactivated player controller and reset camera FOV upon exit
    - Vehicle control disabled when player exits
    - Existing vehicle entry logic remains unchanged

--- The_Trauma/Assets/Scripts/DriveCar.cs
+++ The_Trauma/Assets/Scripts/DriveCar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -10,7 +11,9 @@ public class DriveCar : MonoBehaviour
     public GameObject _Car;
     public GameObject DriveText;
     public GameObject SittingPoint;
+    public GameObject ExitPoint;
     public Camera _Camera;
+    public bool Incar;
 
     //public Animator SittingCar;
 
@@ -19,6 +22,7 @@ public class DriveCar : MonoBehaviour
     private void Start()
     {
         InReach = false;
+        Incar = false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -41,17 +45,38 @@ public class DriveCar : MonoBehaviour
 
     void Update()
     {
-        if (InReach && Input.GetButtonDown("Interact"))
+        if (Input.GetButtonDown("Interact"))
         {
-            //SittingCar.SetBool("Sit", true);
-            _Player.GetComponent<Player>().enabled = false;
-            _Player.GetComponent<CapsuleCollider>().enabled = false;
-            _Player.GetComponent<CharacterController>().enabled = false;
-            _Camera.fieldOfView = 70f;
-            _Car.GetComponent<CarController>().enabled = true;
-            _Player.transform.position = SittingPoint.transform.position;
-            _Player.transform.rotation = SittingPoint.transform.rotation;
-            _Player.transform.SetParent(SittingPoint.transform);
+            if (!Incar && InReach)
+            {
+                Incar = true;
+                _Player.GetComponent<Player>().enabled = false;
+                _Player.GetComponent<CapsuleCollider>().enabled = false;
+                _Player.GetComponent<CharacterController>().enabled = false;
+                _Camera.fieldOfView = 70f;
+                _Car.GetComponent<CarController>().enabled = true;
+
+                _Player.transform.position = SittingPoint.transform.position;
+                _Player.transform.rotation = SittingPoint.transform.rotation;
+                _Player.transform.SetParent(SittingPoint.transform);
+                DriveText.SetActive(false);
+            }
+            else if (Incar)
+            {
+                Incar = false;
+                _Player.transform.SetParent(null);
+
+                _Player.transform.position = ExitPoint.transform.position;
+                Vector3 flatRotation = ExitPoint.transform.eulerAngles;
+                _Player.transform.rotation = Quaternion.Euler(0f, flatRotation.y, 0f);
+
+                _Player.GetComponent<Player>().enabled = true;
+                _Player.GetComponent<CapsuleCollider>().enabled = true;
+                _Player.GetComponent<CharacterController>().enabled = true;
+                _Camera.fieldOfView = 60f;
+                _Car.GetComponent<CarController>().enabled = false;
+
+            }
         }
     }
 }
